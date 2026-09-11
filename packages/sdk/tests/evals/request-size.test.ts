839201e22f7957bcde0a0acbfe9ea24b413510b7
Charlie Lamb <charlielamb20@icloud.com>
2026-09-11 10:52:00 +0100

    Move the sdk request-size test into tests/

--- packages/sdk/tests/evals/request-size.test.ts
+++ packages/sdk/tests/evals/request-size.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "bun:test";
 import type { PublicStartEvalRequest } from "@anpord/schema/public/evals-api";
-import { tooLargeToSubmit } from "./request-size";
+import { tooLargeToSubmit } from "../../src/evals/request-size";
 
 const requestOf = (cases: number, bytesEach: number) =>
   ({
